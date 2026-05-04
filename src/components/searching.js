8ad4ec3407381e55145e062b741b9260b95cf8b7
Arseniy <huycuka9@gmail.com>
2026-05-04 12:11:24 +0300

    shit

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,11 +1,15 @@
-import {rules, createComparison} from "../lib/compare.js";
+import {defaultRules, rules, createComparison} from "../lib/compare.js";
 
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
-
+    const stArr = new Array();
+    const ctArr = new Array();
+    stArr.push('skipEmptyTargetValues');
+    ctArr.push(rules.searchMultipleFields (searchField, ['date', 'customer', 'seller'], false));
+    const compare = createComparison(stArr, ctArr);
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        return data;
+        return data.filter(row => compare(row, state));
     }
-}
+}   
